811a5905d6be9a56f68ea0fb1eea9f3ec39df901
brun0vieira <bar.vieira@campus.fct.unl.pt>
2021-12-25 01:29:55 +0000

    day2

--- 2021/solve01.py
+++ 2021/solve01.py
@@ -1,7 +1,7 @@
 measurements = []
 count = 0
 
-with open('input1.txt') as file:
+with open('input01.txt') as file:
 	for line in file:
 		measurements.append(int(line.rstrip()))
 
@@ -10,3 +10,11 @@ for i in range(len(measurements) - 1):
 		count += 1
 
 print(count)
+
+import numpy as np 
+
+measurements = [int(m) for m in open('input01.txt')]
+windows = np.convolve(measurements, np.ones(3), 'valid')
+
+print(np.sum(np.diff(windows) > 0))
+
--- 2021/solve02.py
+++ 2021/solve02.py
@@ -1,8 +1,26 @@
-#!/usr/bin/env python3
+commands = [(command[0], int(command[1])) for line in open('input02.txt') if (command := line.strip().split(' '))]
 
-import numpy as np
+horizontal, depth = 0, 0 
 
-measurements = [int(m) for m in open('input01.txt')]
-windows = np.convolve(measurements, np.ones(3), 'valid')
+for command, value in commands:
+	if command == 'forward':
+		horizontal += value
+	elif command == 'down':
+		depth += value
+	elif command == 'up':
+		depth -= value
 
-print(np.sum(np.diff(windows) > 0))
+print(horizontal*depth)
+
+horizontal, depth, aim = 0, 0, 0
+
+for command, value in commands:
+	if command == 'forward':
+		horizontal += value
+		depth += aim*value
+	elif command == 'down':
+		aim += value
+	elif command == 'up':
+		aim -= value
+
+print(horizontal*depth)
